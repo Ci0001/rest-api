166bf5d86173f4f07bd453cd4d37f8d8f7263073
Mayur Sorathiya <malabananaic1101@protonmail.com>
2024-05-18 11:22:49 +0530

    Initial commit

---
+++ Controller/Api/BaseController.php
@@ -0,0 +1,46 @@
+<?php
+class BaseController
+{
+    /**
+     * __call magic method
+     * Called when an inaccessible or unknown method is called
+     */
+    public function __call($name, $arguments){
+        $this->sendOutput('',array("HTTP/1.1 404 Not Found"));
+    }
+    /**
+     * GET URI ELEMENTS
+     * @return array
+     */
+    protected function getUriSegments(){
+        $uri = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+        $uri =  explode("/",$uri);
+        return $uri;
+    }
+
+    /**
+     * Get querystring params
+     * @return array
+     */
+    protected function getQueryStringParams()
+    {
+        return parse_str($_SERVER['QUERY_STRING'],$query);
+    }
+    /**
+     * Send API Output
+     * @param mixed $data
+     * @param string $httpHeader
+     */
+    protected function sendOutput($data, $httpHeaders = array())
+    {
+        header_remove('Set-Cookie');
+
+        if(is_array($httpHeaders) && count($httpHeaders)){
+            foreach($httpHeaders as $httpHeader){
+                header($httpHeader);
+            }
+        }
+        echo $data;
+        exit;
+    }
+}
